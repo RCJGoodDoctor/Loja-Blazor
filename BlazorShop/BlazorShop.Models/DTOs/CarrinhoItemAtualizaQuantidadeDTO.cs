93871934c1b80a16716550c9c553ec7418b265b3
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-05 20:24:22 -0300

    Adicionando DTOs

---
+++ BlazorShop/BlazorShop.Models/DTOs/CarrinhoItemAtualizaQuantidadeDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazorShop.Models.DTOs;
+
+public class CarrinhoItemAtualizaQuantidadeDTO
+{
+    [Required]
+    public int CarrinhoItemId { get; set; }
+
+    [Required]
+    public int Quantidade { get; set; }
+
+}
--- BlazorShop/BlazorShop.Web/obj/Debug/net8.0/BlazorShop.Web.AssemblyInfo.cs
+++ BlazorShop/BlazorShop.Web/obj/Debug/net8.0/BlazorShop.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorShop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42378aa8b908638b574e28648b5debaea3e0f6fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorShop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorShop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
