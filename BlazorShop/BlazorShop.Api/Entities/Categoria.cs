42378aa8b908638b574e28648b5debaea3e0f6fc
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-05 13:59:53 -0300

    adicionando projeto incial da loja
    configurado em 3 projetos API (Back-end), Models(Classes) e WEB(blazor)

---
+++ BlazorShop/BlazorShop.Api/Entities/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Api.Entities;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public string Nome { get; set; } = string.Empty;
+    public string IconCSS { get; set; } = string.Empty;
+
+    public Collection<Produto> Produtos { get; set; } = new Collection<Produto>(); // Definição de n:1 de produto para categoria
+}
