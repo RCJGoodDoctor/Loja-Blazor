43d8861e252e0904a6fa00105e3929b3f8d50b40
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-07 09:21:42 -0300

    Produto Repository

---
+++ BlazorShop/BlazorShop.Api/Repositories/IProdutoRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlazorShop.Api.Entities;
+
+namespace BlazorShop.Api.Repositories;
+
+public interface IProdutoRepository
+{
+    public Task<IEnumerable<Produto>> GetItens();
+    public Task<Produto> GetItem(int id);
+    public Task<IEnumerable<Produto>> GetItensByCategory(int id);
+}
--- BlazorShop/BlazorShop.Models/obj/Debug/net8.0/BlazorShop.Models.AssemblyInfo.cs
+++ BlazorShop/BlazorShop.Models/obj/Debug/net8.0/BlazorShop.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorShop.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42378aa8b908638b574e28648b5debaea3e0f6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93871934c1b80a16716550c9c553ec7418b265b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorShop.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorShop.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
